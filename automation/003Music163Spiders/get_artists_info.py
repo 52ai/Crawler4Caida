1af3bdd83aaecf5fe108934f66364927250eca4d
Wayne Yu <ieeflsyu@outlook.com>
2018-11-14 18:35:43 +0800

    update

--- automation/003Music163Spiders/get_artists_info.py
+++ automation/003Music163Spiders/get_artists_info.py
@@ -1,48 +1,44 @@
 # coding:utf-8
-"""
-Create on Nov 13, 2018 by Wayne Yu
-Function: Get music 163 artists info
-主要是温习两件事：第一、静态页面的爬取分析；第二、动态页面的爬取分析。
-注：动态页面的爬取如果能够找到JS加载的方法，就直接采用JS破解，使用本地浏览器测试运行的方法速度一般比较慢。
-=>此次动态页面的爬取采用Python+Selenium+Firefox的组合。（此前使用的是Python+Selenium+PhantomJs的组合）
-Python(3.7) + Selenium(3.141.0) + Firefox(63.0.1)
-=>静态页面的分析使用BeautifulSoup(4.6.3)
-需要下载firefox驱动并放在目录中。（驱动下载地址：https://github.com/mozilla/geckodriver/releases）
-windows:Python的安装目录scripts中，Mac放置在/user/local/bin中
-
-1) selenium的版本要足够高，以适配最新版的firefox浏览器。（起初一直运行失败的原因在于selenium的版本不够高）
-2) 获取加载之后的页面信息，使用的是run_firefox.page_info方法。（至于其他的用法需要摸索，比如运行JS、定位元素等方法）
-3）关于Selenium WebDriver的使用方法，可以参考链接：https://www.seleniumhq.org/docs/03_webdriver.jsp
-4）关于BeautifulSoup的使用方法，可以参考链接：https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.html
-
-"""
-
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
-import re
 import csv
-from selenium import webdriver
-import time
 
 
-def get_artists_info(url):
-    """
-    抓取网易云音乐歌手页面的动态信息，并根据要求返回相关信息
-    :param url:
-    :return:artists_info
-    """
-    # html = urlopen(url)
-    run_firefox = webdriver.Firefox()
-    run_firefox.get(url)
-    time.sleep(5)
-    # print(run_firefox.page_source)
-    bs_obj = BeautifulSoup(run_firefox.page_source, "html5lib")
-    print(bs_obj)
-    run_firefox.close()
-    return "OK!"
+# 构造函数获取歌手信息
+def get_artists(url):
+    headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+             'Accept-Encoding': 'gzip, deflate',
+             'Accept-Language': 'zh-CN,zh;q=0.9',
+             'Connection': 'keep-alive',
+             'Cookie': '_iuqxldmzr_=32; _ntes_nnid=0e6e1606eb78758c48c3fc823c6c57dd,1527314455632; '
+                       '_ntes_nuid=0e6e1606eb78758c48c3fc823c6c57dd; __utmc=94650624; __utmz=94650624.1527314456.1.1.'
+                       'utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WM_TID=blBrSVohtue8%2B6VgDkxOkJ2G0VyAgyOY;'
+                       ' JSESSIONID-WYYY=Du06y%5Csx0ddxxx8n6G6Dwk97Dhy2vuMzYDhQY8D%2BmW3vlbshKsMRxS%2BJYEnvCCh%5CKY'
+                       'x2hJ5xhmAy8W%5CT%2BKqwjWnTDaOzhlQj19AuJwMttOIh5T%5C05uByqO%2FWM%2F1ZS9sqjslE2AC8YD7h7Tt0Shufi'
+                       '2d077U9tlBepCx048eEImRkXDkr%3A1527321477141; __utma=94650624.1687343966.1527314456.1527314456'
+                       '.1527319890.2; __utmb=94650624.3.10.1527319890',
+             'Host': 'music.163.com',
+             'Referer': 'http://music.163.com/',
+             'Upgrade-Insecure-Requests': '1',
+             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                           'Chrome/66.0.3359.181 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html5lib')
+    for artist in soup.find_all('a', attrs={'class': 'nm nm-icn f-thide s-fc0'}):
+        artist_name = artist.string
+        artist_id = artist['href'].replace('/artist?id=', '').strip()
+        try:
+            writer.writerow((artist_id, artist_name))
+        except Exception as msg:
+            print(msg)
 
 
-if __name__ == "__main__":
-    page_url = "https://music.163.com/#/artist?id=8103"
-    # page_url = "https://xueqiu.com/7318086163/116753217"
-    print(get_artists_info(page_url))
+ls1 = [1001, 1002, 1003, 2001, 2002, 2003, 6001, 6002, 6003, 7001, 7002, 7003, 4001, 4002, 4003]    # id的值
+ls2 = [-1, 0, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]   # initial的值
+csvfile = open('./music_163_artists.csv', 'a')    # 文件存储的位置
+writer = csv.writer(csvfile)
+writer.writerow(('artist_id', 'artist_name'))
+for i in ls1:
+    for j in ls2:
+        url = 'http://music.163.com/discover/artist/cat?id=' + str(i) + '&initial=' + str(j)
+        get_artists(url)
