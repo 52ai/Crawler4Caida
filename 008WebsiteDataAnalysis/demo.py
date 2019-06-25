4d85d5c5050713e1eabbb9494f452a424209fa88
Wayne Yu <ieeflsyu@outlook.com>
2019-06-25 17:40:45 +0800

    Package python file to exe file lightly, using pyinstaller and pipenv!

--- 008WebsiteDataAnalysis/demo.py
+++
@@ -1,6 +0,0 @@
-# coding:utf-8
-
-list1 = ["1", "2", "3"]
-
-for n in list1:
-    print(n)
---
+++ 008WebsiteDataAnalysis/exe_demo.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+"""
+create on June 25, 2019 by Wayne Yu
+Function:使用pyinstaller和pipenv，实现轻量级打包Python程序为exe可执行文件
+"""
+import time
+print("This is my first python2exe program.")
+print("Time:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())))
+print("Author: Wayne Yu")
+print("Package Python file to exe file lightly, using pyintaller and pipenv! ")
+time.sleep(30)
