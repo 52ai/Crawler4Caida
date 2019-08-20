b6772c6f98c0a2e0c5aca3db1cbb211c1cf40a60
Wayne Yu <ieeflsyu@outlook.com>
2019-08-20 19:06:04 +0800

    hello,Tkinter!

--- 010GainWebsiteInfo/deploy/README.md
+++ 010GainWebsiteInfo/deploy/README.md
@@ -8,4 +8,4 @@
 >content.csv->输入的网站列表（若采集新的网站列表数据,按照给定格式替换该文件中的记录即可）<br>
 exe_log.log->网址预处理后的运行记录<br>
 website_n_screen.png->第n个运行记录的网站截图<br>
-website_n_source.png->第n个运行记录的网页源码<br>
+website_n_source.html->第n个运行记录的网页源码<br>
---
+++ 012Tkinter/helloTkinter.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+"""
+Create on 20 Aug,2019
+"""
+
+import tkinter as tk
+
+root = tk.Tk()  # 创建一个主窗口，用于容纳整个GUI程序
+root.title("NEW")  # 设置主窗口对象的标题栏
+
+the_lable = tk.Label(root, text="Hello，Python GUI---Tkinter!")  # 添加一个Label组件，Label可以显示文本、图标或图片
+the_lable.pack()  # 调用Label组件的pack()方法，用于自动调节组件自身尺寸
+root.mainloop()
