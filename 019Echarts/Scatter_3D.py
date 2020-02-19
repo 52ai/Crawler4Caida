c6356a8ce48c944bcde52353411f15ef1e52ead1
wayne <ieeflsyu@outlook.com>
2020-02-19 11:52:43 +0800

    继续研究可视化

--- 019Echarts/Scatter_3D.py
+++ 019Echarts/Scatter_3D.py
@@ -57,31 +57,45 @@ def read_as_info(file_name):
 
 
 def scatter3d_base() -> Scatter3D:
-    # data = [
-    #     [random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100)]
-    #     for _ in range(1000)
-    # ]
+    data = [
+        [random.randint(-10000, 10000), random.randint(-10000, 10000), random.randint(-10000, 10000)]
+        for _ in range(100)
+    ]
     # 求莫比乌斯环数据
-    v_mobius = np.linspace(-1.0, 1.0, num=500, endpoint=True)
-    u_mobius = np.linspace(0, 2 * np.pi, num=500, endpoint=True)
-    x_3d = (1. + v_mobius/2. * np.cos(u_mobius/2.)) * np.cos(u_mobius)
-    print(x_3d)
-    y_3d = (1. + v_mobius/2. * np.cos(u_mobius/2.)) * np.sin(u_mobius)
-    print(y_3d)
-    z_3d = v_mobius/2. * np.sin(v_mobius/2.)
-    print(z_3d)
+    # v_mobius = np.linspace(-1.0, 1.0, num=20, endpoint=True)
+    # u_mobius = np.linspace(0, 2 * np.pi, num=20, endpoint=True)
+    # x_3d = (1. + v_mobius/2. * np.cos(u_mobius/2.)) * np.cos(u_mobius)
+    # print(x_3d)
+    # y_3d = (1. + v_mobius/2. * np.cos(u_mobius/2.)) * np.sin(u_mobius)
+    # print(y_3d)
+    # z_3d = v_mobius/2. * np.sin(v_mobius/2.)
+    # print(z_3d)
     # data = [[0, 0, 0], [100, 100, 100], [100, -100, -100]]
     # print(data)
-    data = []
-    temp_list = []
-    for iter_cnt in range(0, len(list(x_3d))):
-        temp_list.append(x_3d[iter_cnt])
-        temp_list.append(y_3d[iter_cnt])
-        temp_list.append(x_3d[iter_cnt])
-        data.append(temp_list)
-        temp_list = []
+    # data = []
+    # temp_list = []
+    # for iter_cnt in range(0, len(list(x_3d))):
+    #     temp_list.append(x_3d[iter_cnt])
+    #     temp_list.append(y_3d[iter_cnt])
+    #     temp_list.append(x_3d[iter_cnt])
+    #     data.append(temp_list)
+    #     temp_list = []
     print(data)
     # data, max_rel = read_as_info('..\\000LocalData\\as_compare\\as_core_map_data_integrate20191203.csv')
+    assert isinstance(
+        Scatter3D(init_opts=opts.InitOpts(width="1920px", height="960px", page_title="3D散点图", theme=ThemeType.DARK))
+        .add("3D散点可视化",
+             data,
+             grid3d_opts=opts.Grid3DOpts(width=100, height=100, depth=100, rotate_speed=5, is_rotate=False,
+                                         rotate_sensitivity=2),
+             xaxis3d_opts=opts.Axis3DOpts(type_="value", name="x", textstyle_opts=opts.TextStyleOpts(color="white")),
+             yaxis3d_opts=opts.Axis3DOpts(type_="value", name="y", textstyle_opts=opts.TextStyleOpts(color="white")),
+             zaxis3d_opts=opts.Axis3DOpts(type_="value", name="z", textstyle_opts=opts.TextStyleOpts(color="white")),
+             itemstyle_opts=opts.ItemStyleOpts(color="white", border_width=0.01, border_color="red", area_color="blue"),
+             # label_opts=opts.LabelOpts(color="objectwhite")
+
+             )
+        .set_global_opts, )
     c = (
         Scatter3D(init_opts=opts.InitOpts(width="1920px", height="960px", page_title="3D散点图", theme=ThemeType.DARK))
         .add("3D散点可视化",
@@ -90,13 +104,15 @@ def scatter3d_base() -> Scatter3D:
              xaxis3d_opts=opts.Axis3DOpts(type_="value", name="x", textstyle_opts=opts.TextStyleOpts(color="white")),
              yaxis3d_opts=opts.Axis3DOpts(type_="value", name="y", textstyle_opts=opts.TextStyleOpts(color="white")),
              zaxis3d_opts=opts.Axis3DOpts(type_="value", name="z", textstyle_opts=opts.TextStyleOpts(color="white")),
-             itemstyle_opts=opts.ItemStyleOpts(),
+             itemstyle_opts=opts.ItemStyleOpts(color="white", border_width=0.01, border_color="red", area_color="blue"),
+
              # label_opts=opts.LabelOpts(color="white")
 
              )
         .set_global_opts(
             title_opts=opts.TitleOpts("3D散点图"),
-            visualmap_opts=opts.VisualMapOpts(range_color=Faker.visual_color),
+            # visualmap_opts=opts.VisualMapOpts(range_color=Faker.visual_color),
+
         )
     )
     print(data)
