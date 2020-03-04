010c9639900688fc18dbb064d99a1bb67c0ff77c
wayne <ieeflsyu@outlook.com>
2020-03-04 23:24:43 +0800

    全面梳理院大屏系统所需绘图数据

---
+++ 031CAICT-Display/display_data.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+
+"""
+create on Mar 4, 2020 By Wenyan YU
+Email: ieeflsyu@outlook.com
+
+Function:
+
+针对院大屏系统所需的绘图数据，做统一的集成处理，实现一键出数据。
+
+1）全球BGP互联关系变化趋势（时间+统计值，包括All Relationships、Peer、Transit、Transit as Provider、Transit as Customer）
+2）全球BGP互联关系数据量统计分布图（互联数量+统计值，无标度网络特征出现，后续再进一步研究）（直接绘制1998-2019）
+3）全球各主要国家国内BGP互联关系变化趋势（时间+国内BGP互联关系数量）
+4）全球各主要国家对外BGP互联关系变化趋势（时间+对外BGP互联关系数量）
+5）代表性AS互联关系变化趋势（全球TOP AS 20、全国TOP AS 20，时间+BGP互联关系统计值5项）
+6）我国出口AS互联关系统计（互联国家+互联关系总数）（直接绘制1998-2019）
+
+"""
+
+
+
