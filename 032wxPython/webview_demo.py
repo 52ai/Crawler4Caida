ddf796c380bbae8ba8af6a892c0e05cbd38e2c75
wayne <ieeflsyu@outlook.com>
2020-03-06 19:39:25 +0800

    Python UI, 温故而知新

---
+++ 032wxPython/webview_demo.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+"""
+create on Mar 6, 2020 By Wenyan YU
+Email: ieeflsyu@outlook.com
+
+Function:
+
+尝试着在wxpython中内嵌浏览器
+
+"""
+import wx
+from wx.html2 import WebView
+
+
+class MyHtmlFrame(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, -1, title, size=(1024, 768))
+        web_view = WebView.New(self)
+        web_view.LoadURL("http://mryu.top/apps/graph_cjk_gao.html")
+
+
+app = wx.App()
+frm = MyHtmlFrame(None, "Browser")
+frm.Show()
+app.MainLoop()
