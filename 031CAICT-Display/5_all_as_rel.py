8256ac8667be61697d511864930a6f6a6934aeb7
wayne <ieeflsyu@outlook.com>
2020-05-09 13:55:46 +0800

    继续地图基础课题

--- 031CAICT-Display/5_all_as_rel.py
+++ 031CAICT-Display/5_all_as_rel.py
@@ -5,9 +5,22 @@ create on May 8, 2020 By Wayne YU
 Function:
 
 统计全球各主要国家从1998-2019，国家全部互联关系的变化趋势（分为转接互联（Transit）和对等互联(Peer)）
+统计网络排名前20名的国家（AS号码的分配）
+
+美国、巴西、俄罗斯、德国、英国、
+澳大利亚、波兰、印度、乌克兰、加拿大、
+法国、印度尼西亚、中国、荷兰、罗马尼亚
+意大利、西班牙、阿根廷、中国香港、孟加拉国
+
+瑞士、韩国、日本、瑞典、保加利亚、
+捷克、土耳其、伊朗、奥地利、新西兰、
+南非、新加坡、泰国、墨西哥、菲律宾、
+丹麦、挪威、芬兰、比利时、越南、
+
+country = ["US", "BR", "RU", "DE", "GB", "AU", "PL", "IN", "UA", "CA",
+            "FR", "ID", "CN", "NL", "RO", "IT", "ES", "AR", "HK", "BD", "CH", "KR", "JP", "SE", "BG"]
 
 """
-
 import time
 import csv
 import os
@@ -33,39 +46,28 @@ def write_to_csv(res_list, des_path):
     print("write finish!")
 
 
-def gain_as2country(as_info_file, country):
+def gain_as2country(as_info_file):
     """
-    根据传入的as info file信息获取AS与国家的对应字典及该国家的所有的AS Info
+    根据传入的as info file信息获取AS与国家的对应字典
     :param as_info_file:
-    :param country:
-    :return country_as_info:
     :return as2country:
     """
-    country_as_info = []  # 存储country as 信息
     as2country = {}  # 存储as号到country的映射关系
     file_read = open(as_info_file, 'r', encoding='utf-8')
     for line in file_read.readlines():
         line = line.strip().split("\t")
         # print(line)
         as_number = line[0]
-        as_name = line[1].strip().split(",")[0].strip()
         as_country = line[1].strip().split(",")[-1].strip()
         as2country[as_number] = as_country
-        temp_list = []
-        if as_country == country:
-            temp_list.append(as_number)
-            temp_list.append(as_name)
-            temp_list.append(as_country)
-            country_as_info.append(temp_list)
 
-    return country_as_info, as2country
+    return as2country
 
 
-def external_as_analysis(country, country_as_info, as2country):
+def external_as_analysis(country, as2country):
     """
     根据输入的国家，统计该国家的出口AS数量及其互联方向的统计分析
     :param country:
-    :param country_as_info:
     :param as2country:
     :return:
     """
@@ -80,37 +82,48 @@ def external_as_analysis(country, country_as_info, as2country):
     temp_list = []
     for path_item in file_path:
         print(path_item)
-        # 遍历一次文件，获取该国出口AS的数量
+        # 遍历一次文件，获取该国AS参与互联的关系，并对，Transit和Peer关系进行分类，包含国内、国外
         file_read = open(path_item, 'r', encoding='utf-8')
-        internal_cnt = 0  # 存储该国内部连边的数量
+        all_cnt = 0  # 存储该国AS所有的互联关系
+        peer_cnt = 0  # 存储该国AS所有互联关系中对等互联的数量
+        transit_cnt = 0  # 存储该国AS所有互联关系中转接互联的数量
         for line in file_read.readlines():
             if line.strip().find("#") == 0:
                 continue
             try:
-                if as2country[str(line.strip().split('|')[0])] == country:
-                    if as2country[str(line.strip().split('|')[1])] == country:
-                        internal_cnt += 1
+                as0_country = as2country[str(line.strip().split('|')[0])]
+                as1_country = as2country[str(line.strip().split('|')[1])]
+                if (as0_country == country) or (as1_country == country):
+                    all_cnt += 1
+                    if str(line.strip().split('|')[2]) == "0":
+                        peer_cnt += 1
+                    else:
+                        transit_cnt += 1
             except Exception as e:
+                # print(e)
                 pass
 
-        print("internal Edges Count:", internal_cnt)
+        print("ALL REL|PEER|TRANSIT:", all_cnt, peer_cnt, transit_cnt)
         temp_str = path_item.split('\\')[-1]
         date_str = temp_str.split('.')[0]
         temp_list.append(date_str)
-        temp_list.append(internal_cnt)
+        temp_list.append(all_cnt)
+        temp_list.append(peer_cnt)
+        temp_list.append(transit_cnt)
         print(temp_list)
         return_list.append(temp_list)
         temp_list = []
-    save_path = "../000LocalData/caict_display/internal_BGP_Rel_" + country + ".csv"
+    save_path = "../000LocalData/caict_display/All_BGP_Rel_" + country + ".csv"
     write_to_csv(return_list, save_path)
 
 
 if __name__ == "__main__":
     time_start = time.time()  # 记录启动时间
-    country = ["CN", "US", "RU", "JP", "KR"]
+    country = ["US", "BR", "RU", "DE", "GB", "AU", "PL", "IN", "UA", "CA",
+               "FR", "ID", "CN", "NL", "RO", "IT", "ES", "AR", "HK", "BD", "CH", "KR", "JP", "SE", "BG"]
     as_info_file_in = '..\\000LocalData\\as_Gao\\asn_info.txt'
-    country_as_info, as2country_dict = gain_as2country(as_info_file_in, country)
+    as2country_dict = gain_as2country(as_info_file_in)
     for country_item in country:
-        external_as_analysis(country_item, country_as_info, as2country_dict)
+        external_as_analysis(country_item, as2country_dict)
     time_end = time.time()
     print("=>Scripts Finish, Time Consuming:", (time_end - time_start), "S")
