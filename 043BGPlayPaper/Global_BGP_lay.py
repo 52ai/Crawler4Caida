df9cd18d3c52a41d00bec19b0f86ec219a44057f
wayne <ieeflsyu@outlook.com>
2020-06-11 16:15:37 +0800

    网络拓扑大数据图的三个优化方向可能没有想象中的那么简单

--- 043BGPlayPaper/Global_BGP_lay.py
+++ 043BGPlayPaper/Global_BGP_lay.py
@@ -7,7 +7,7 @@ create on June 9, 2020 By Wayne Yu
 
 三个可能的优化的方向
 
-1）舍去连线，参考The Internet Mao的形式，绘制全球AS级网络互联关系地图
+1）舍去连线，参考The Internet Map的形式，绘制全球AS级网络互联关系地图
 2）研究机器学习聚类算法，寻找合适的聚类模式，或者改进之
 3）提前输出点的静态坐标，再行绘制降低实时计算量
 
@@ -27,6 +27,35 @@ repulsion=5,
 画出的图，还可以，比之前散开的图要好看些，大体能充满画布，没有那么空
 在进行该实验的过程中解决了程序自动生成高清图的问题，并实现了自动化绘制多年的对比星云图，可从时间维度上进行对比
 
+解决了pyecharts资源本地引用的问题
+通过自定义主题解决了不同类别的节点颜色自定义问题
+
+中日韩 repulsion=20
+中东盟 repulsion=15
+中国（大陆+港澳台）repulsion=30
+中德（2440Nodes, 4521Links） repulsion=15
+中美（17718Nodes, 40476Links） repulsion=5 【用小黑跑直接Time Out，需要GPU加速啊】
+中英（2647Nodes, 4745Links） repulsion=15
+中法（1628Nodes, 3799Links） repulsion=20
+
+# 实验2 舍去连线，绘制类The Internet Map的图（依然采用国家（地区）Group组团的形式）（20200610）
+不需要再新建文件，直接编写新的绘制函数即可
+经实验研究，echarts现有的力引导算法并不是很好的适用绘制The Internet Map图，需要根据自有力引导算法进行相应的改进
+因此可以三个可能的优化方向中第三点结合起来，利用自有改进的力引导算法，计算并输出静态坐标。
+再利用echarts的可视化能力，进行JS效果的网络图展示
+
+# 实验3 研究机器学习聚类算法，从中寻找灵感
+聚类算法是机器学习中涉及对数据进行分组的一种算法，在给定的数据集中，我们可以通过聚类算法将其分成一些不同的组。
+在理论上，相同组的数据之间有相同的属性或者特征，不同组数据之间的属性或者特征相差比较大
+聚类算法是一种非监督学习算法，并且作为常用的数据分析算法在很多领域上得到应用
+
+K-Means
+Mean-Shift
+DBSCAN（基于密度的带噪声的空间聚类的应用）
+基于高斯混合模型（GMM）的期望最大化（EM）聚类
+凝聚层次聚类
+图团体检测（Graph Community Detection）
+
 """
 import json
 import numpy as np
@@ -44,7 +73,7 @@ from snapshot_selenium import snapshot
 # 引用本地静态资源
 CurrentConfig.ONLINE_HOST = "http://127.0.0.1:8000/"
 # 使用自己构建的主题
-# register_files({"myTheme": ["themes/myTheme", "js"]})
+register_files({"wayne": ["themes/wayne", "js"]})
 
 
 def write_to_csv(res_list, des_path):
@@ -88,13 +117,13 @@ def gain_country_info():
     :return country_info_dict:
     """
     geo_file = '../000LocalData/as_geo/GeoLite2-Country-Locations-zh-CN.csv'
-    coutry_info_dict = {}
+    country_info_dict = {}
     file_read = open(geo_file, 'r', encoding='utf-8')
     for line in file_read.readlines():
         line = line.strip().split(',')
         # print(line)
-        coutry_info_dict[line[4]] = line[5]
-    return coutry_info_dict
+        country_info_dict[line[4]] = line[5]
+    return country_info_dict
 
 
 def read_as_info(file_name, en2cn_country):
@@ -111,7 +140,7 @@ def read_as_info(file_name, en2cn_country):
     temp_dict_label = {}
     file_read = open(file_name, 'r', encoding='utf-8')
     as_list = []  # 记录所有展示的as列表
-    country_group = ["中国（大陆）", "日本", "俄罗斯", "中国（香港）"]
+    country_group = ["中国（大陆）", "法国"]
     # country_group_color = dict()
     # country_group_color["中国（大陆）"] = "red"
     # country_group_color["日本"] = "green"
@@ -203,7 +232,58 @@ def graph_as_lay(title_name, country_en2cn, time_str) -> Graph:
         Graph(init_opts=opts.InitOpts(width="1000px",
                                       height="691.883px",
                                       page_title=title_name,
-                                      theme=ThemeType.DARK,
+                                      theme="wayne",
+                                      bg_color="#000"))
+        .add(
+            series_name="",
+            nodes=as_info_dict,
+            links=as_links_dict,
+            categories=categories_dict,
+            # layout="circular",
+            is_rotate_label=True,
+            gravity=1,
+            repulsion=20,
+            linestyle_opts=opts.LineStyleOpts(width=0.5, opacity=0.3, color='source', curve=0),
+            label_opts=opts.LabelOpts(is_show=False, font_size=8),
+        )
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(textstyle_opts=opts.TextStyleOpts(font_size=14),
+                                        legend_icon='roundRect')
+                   )
+        )
+    return c
+
+
+def graph_as_lay_no_line(title_name, country_en2cn, time_str) -> Graph:
+    """
+    尝试着不会绘制连线
+    :param title_name:
+    :param country_en2cn:
+    :param time_str:
+    :return:
+    """
+    file_in = '..\\000LocalData\\as_map\\as_core_map_data_new' + time_str + '1001.csv'
+    bgp_file = "..\\000LocalData\\as_relationships\\serial-1\\" + time_str + "1001.as-rel.txt"
+    as_info_dict, as_list, categories_dict = read_as_info(file_in, country_en2cn)
+    print("Nodes:", len(as_info_dict))
+    print("Categories:", len(categories_dict))
+    print("As List:", len(as_list))
+    as_links_dict = read_as_links(bgp_file, as_list)
+    print("links:", len(as_links_dict))
+    out_json = list()
+    out_json.append(as_info_dict)
+    out_json.append(as_links_dict)
+    out_json.append(categories_dict)
+    final_json = json.dumps(out_json, indent=4)
+    with open("..\\000LocalData\\BGPlay\\Global_BGP_lay.json", 'a') as f:
+        f.write(final_json)
+
+    title_name = title_name + "[Nodes:" + str(len(as_info_dict)) + " Links:" + str(len(as_links_dict)) + "]"
+    c = (
+        Graph(init_opts=opts.InitOpts(width="1000px",
+                                      height="691.883px",
+                                      page_title=title_name,
+                                      theme="wayne",
                                       bg_color="#000"))
         .add(
             series_name="",
@@ -214,7 +294,7 @@ def graph_as_lay(title_name, country_en2cn, time_str) -> Graph:
             is_rotate_label=True,
             gravity=1,
             repulsion=5,
-            linestyle_opts=opts.LineStyleOpts(width=0.5, opacity=0.3, color='source', curve=0),
+            linestyle_opts=opts.LineStyleOpts(width=0, opacity=0.3, color='source', curve=0),
             label_opts=opts.LabelOpts(is_show=False, font_size=8),
         )
         .set_global_opts(
@@ -230,15 +310,14 @@ if __name__ == "__main__":
     my_time_str = ["1999", "2004", "2009", "2014", "2019"]
     my_country_en2cn = gain_country_info()
     opt_title_name = "Graph-全球AS网络互联关系拓扑图"
-    for str_item in my_time_str:
+    for str_item in my_time_str[-1:]:
         render_str = "..\\000LocalData\\BGPlay\\Global_BGP_lay" + str_item + ".html"
         img_str = "..\\000LocalData\\BGPlay\\Global_BGP_lay" + str_item + ".jpeg"
         make_snapshot(snapshot,
-                      graph_as_lay(opt_title_name,
-                                   my_country_en2cn,
-                                   str_item).render(render_str),
+                      graph_as_lay(opt_title_name, my_country_en2cn, str_item).render(render_str),
                       img_str,
-                      delay=30, pixel_ratio=10)
+                      delay=30, pixel_ratio=12)
+
         print(img_str)
     time_end = time.time()
     print("=>Scripts Finish, Time Consuming:", (time_end - time_start), "S")
