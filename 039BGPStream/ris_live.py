31afd32a7286c2a78ade8a18b2818cd1898b81c9
Wenyan YU <ieeflsyu@outlook.com>
2020-09-09 18:46:27 +0800

    学会复盘

--- 039BGPStream/ris_live.py
+++ 039BGPStream/ris_live.py
@@ -10,23 +10,41 @@ Require 'websocket-client'
 """
 import json
 import websocket
+import time
 
-ws = websocket.WebSocket()
-ws.connect("wss://ris-live.ripe.net/v1/ws/?client=py-example-1")
 
-params = {
-    "moreSpecific": True,
-    "host": "rrc24",
-    "socketOptions": {
-        "includeRaw": True
+def gain_live_data():
+    """
+    获取实时数据
+    :return:
+    """
+    ws = websocket.WebSocket()
+    ws.connect("wss://ris-live.ripe.net/v1/ws/?client=py-example-1")
+
+    params = {
+        "moreSpecific": True,
+        "host": "rrc24",
+        "socketOptions": {
+            "includeRaw": True
+        }
     }
-}
-ws.send(json.dumps({
-    "type": "ris_subscribe",
-    "data": params
-}))
 
-for data in ws:
-    parsed = json.loads(data)
-    print(parsed["type"], parsed["data"])
+    ws.send(json.dumps({
+        "type": "ris_subscribe",
+        "data": params
+    }))
 
+    for data in ws:
+        parsed = json.loads(data)
+        print(parsed["type"], parsed["data"])
+
+
+if __name__ == "__main__":
+    time_start = time.time()
+    while True:
+        try:
+            gain_live_data()
+        except Exception as e:
+            print(e)
+        time_end = time.time()
+        print("=>Scripts Finish, Time Consuming:", (time_end - time_start), "S")
