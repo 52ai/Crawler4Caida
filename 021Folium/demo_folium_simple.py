ccc02a3342a84e236354be15a9ec084ad4ca1bbe
Wayne YU <ieeflsyu@outlook.com>
2021-02-23 19:02:55 +0800

    更新统计中国活跃自治域网络数量的程序

--- 021Folium/demo_folium_simple.py
+++ 021Folium/demo_folium_simple.py
@@ -17,6 +17,9 @@ m = folium.Map(location=[latitude, longitude], zoom_start=4)
 tooltip = 'Click me!'
 folium.Marker([30.54, 114.15], popup='<i>AS4134</i>', tooltip=tooltip).add_to(m)
 folium.Marker([39.89, 116.41], popup='<b>AS4847</b>', tooltip=tooltip).add_to(m)
-plugins.PolyLineOffset([[30.54, 114.15], [39.89, 116.41]]).add_to(m)
+folium.Marker([35.61, 139.05], popup='<b>AS173</b>', tooltip=tooltip).add_to(m)
+
+plugins.PolyLineOffset([[30.54, 114.15], [39.89, 116.41]], color='green').add_to(m)
+plugins.PolyLineOffset([[39.89, 116.41], [35.61, 139.05]], color='red').add_to(m)
 
 m.save("as_geo_map.html")
