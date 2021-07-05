90f372b217d18243e63ac680d4796a181e24c659
Wayne YU <ieeflsyu@outlook.com>
2021-07-05 10:35:52 +0800

    今日完成全球IX星云图的初步绘制

--- 068IXVis/ix_rel.py
+++ 068IXVis/ix_rel.py
@@ -14,3 +14,27 @@ Function:
 形成ix组，net组，以及ix-net的关系
 
 """
+from urllib.request import urlopen
+import json
+from datetime import *
+import time
+
+
+def generate_ix_rel():
+    """
+    基于PEERING DB数据抽取ix和net的对应关系
+    :return:
+    """
+    html = urlopen(r"https://www.peeringdb.com/api/netixlan")
+    html_json = json.loads(html.read())
+    for item in html_json['data']:
+        print(item)
+
+
+if __name__ == "__main__":
+    time_start = time.time()
+    generate_ix_rel()
+    print("=>Scripts Finish, Time Consuming:", (time.time() - time_start), "S")
+
+
+
