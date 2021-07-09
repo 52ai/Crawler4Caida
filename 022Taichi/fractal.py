8039a5526687ec6759d46d65da7da143474ad6d9
Wayne YU <ieeflsyu@outlook.com>
2021-07-09 20:22:25 +0800

    Taichi是最好的榜样

--- 022Taichi/fractal.py
+++ 022Taichi/fractal.py
@@ -4,29 +4,93 @@ create on Jan 14,2020 By Wayne YU
 Function: Test Taichi
 """
 
+# Water wave effect partially based on shallow water equations
+# https://en.wikipedia.org/wiki/Shallow_water_equations#Non-conservative_form
+
 import taichi as ti
 
-n = 320
-pixels = ti.var(dt=ti.f32, shape=(n * 2, n))
+ti.init(arch=ti.gpu)
 
-@ti.func
-def complex_sqr(z):
-  return ti.Vector([z[0] * z[0] - z[1] * z[1], z[1] * z[0] * 2]) # z^2
+light_color = 1
+gravity = 2.0  # larger gravity makes wave propagates faster
+damping = 0.2  # larger damping makes wave vanishes faster when propagating
+dx = 0.02
+dt = 0.01
+shape = 512, 512
+pixels = ti.field(dtype=float, shape=shape)
+background = ti.field(dtype=float, shape=shape)
+height = ti.field(dtype=float, shape=shape)
+velocity = ti.field(dtype=float, shape=shape)
+
 
 @ti.kernel
-def paint(t: ti.f32):
-  for i, j in pixels: # Parallized over all pixels
-    c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-    z = ti.Vector([float(i) / n - 1, float(j) / n - 0.5]) * 2
-    iterations = 0
-    while z.norm() < 20 and iterations < 50:
-      z = complex_sqr(z) + c
-      iterations += 1
-    pixels[i, j] = 1 - iterations * 0.02
+def reset():
+    for i, j in height:
+        t = i // 16 + j // 16
+        if t % 2 == 0:
+            background[i, j] = 0.0
+        else:
+            background[i, j] = 0.25
+        height[i, j] = 0
+        velocity[i, j] = 0
 
-gui = ti.GUI("Julia Set", res=(n * 2, n))
 
-for i in range(1000000):
-  paint(i * 0.03)
-  gui.set_image(pixels)
-  gui.show()
+@ti.func
+def laplacian(i, j):
+    return (-4 * height[i, j] + height[i, j - 1] + height[i, j + 1] +
+            height[i + 1, j] + height[i - 1, j]) / (4 * dx**2)
+
+
+@ti.func
+def gradient(i, j):
+    return ti.Vector([
+        height[i + 1, j] - height[i - 1, j],
+        height[i, j + 1] - height[i, j - 1]
+    ]) * (0.5 / dx)
+
+
+@ti.kernel
+def create_wave(amplitude: ti.f32, x: ti.f32, y: ti.f32):
+    for i, j in ti.ndrange((1, shape[0] - 1), (1, shape[1] - 1)):
+        r2 = (i - x)**2 + (j - y)**2
+        height[i, j] = height[i, j] + amplitude * ti.exp(-0.02 * r2)
+
+
+@ti.kernel
+def update():
+    for i, j in ti.ndrange((1, shape[0] - 1), (1, shape[1] - 1)):
+        acceleration = gravity * laplacian(i, j) - damping * velocity[i, j]
+        velocity[i, j] = velocity[i, j] + acceleration * dt
+        height[i, j] = height[i, j] + velocity[i, j] * dt
+
+
+@ti.kernel
+def visualize_wave():
+    # visualizes the wave using a fresnel-like shading
+    # a brighter color indicates a steeper wave
+    # (closer to grazing angle when looked from above)
+    for i, j in pixels:
+        g = gradient(i, j)
+        cos_i = 1 / ti.sqrt(1 + g.norm_sqr())
+        brightness = pow(1 - cos_i, 2)
+        color = background[i, j]
+        pixels[i, j] = (1 - brightness) * color + brightness * light_color
+
+
+print("[Hint] click on the window to create waves")
+
+reset()
+gui = ti.GUI('Water Wave', shape)
+while gui.running:
+    for e in gui.get_events(ti.GUI.PRESS):
+        if e.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
+            gui.running = False
+        elif e.key == 'r':
+            reset()
+        elif e.key == ti.GUI.LMB:
+            x, y = e.pos
+            create_wave(3, x * shape[0], y * shape[1])
+    update()
+    visualize_wave()
+    gui.set_image(pixels)
+    gui.show()
