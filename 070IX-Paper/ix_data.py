08854857b5ec65ff79aa982120042a5c1c69905e
Wayne YU <ieeflsyu@outlook.com>
2021-10-15 18:31:01 +0800

    想法子搞paper啊

---
+++ 070IX-Paper/ix_data.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+"""
+create on Oct 15, 2021 By Wenyan YU
+Email: ieeflsyu@outlook.com
+
+Function:
+
+近来做了很多IX方面的数据研究和可视化尝试，可以适时输出一篇sci论文了
+目前的想法是从时间维度上，研究IX的发展对于全球互联网发展的影响
+包括拓扑结构、IX可视化（现状与时序）、IX与其他方的关联分析（ASes、Tier 1、海缆、DNS根和顶级等网络基础设施）
+IX 与 大型互联网企业（ICP、云、CDN、IDC等）
+IX 与 大型运营商网络（Tier1、Tier2）
+IX 与 小型互联网企业
+IX 与 小型运营商网络（ISP）
+IX 与 海缆
+IX 与 路由
+IX 与 DNS根和顶级
+IX 与 互联网架构
+IX 与 可视化
+IX 与 数据获取
+IX 与 区域发展模式
+IX 与 发展趋势（时间序列）
+IX 与 图模型
+IX 与 复杂网络
+"""
