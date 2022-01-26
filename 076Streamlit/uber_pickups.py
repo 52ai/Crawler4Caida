beff5f00d460ce5d6d84225a9aae6eb60b633e54
Wayne YU <ieeflsyu@outlook.com>
2022-01-26 22:40:27 +0800

    MachineEyes完成基本框架，后续再丰富完善

--- 076Streamlit/uber_pickups.py
+++ 076Streamlit/uber_pickups.py
@@ -45,7 +45,7 @@ if 'authentication_status' not in st.session_state:
 
 st.session_state.menu = selected
 st.write("You select %s Menu" % selected)
-st.write("Session:", st.session_state)
+# st.write("Session:", st.session_state)
 
 names = ['John Smith', 'Rebecca Briggs']
 usernames = ['jsmith', 'rbriggs']
@@ -63,17 +63,133 @@ elif authentication_status is None:
 
 
 if selected == '48hours':
-    str_markdown = "##### GoCN 每日新闻（2022-01-04）\n" \
-                   "1. [「GoCN 酷 Go 推荐」go 语言位操作库 — bitset](https://mp.weixin.qq.com/s/UcuKgKnt4fwrg3c-UHc3sw) \n" \
-                   "2. [Go 通过 Map/Filter/ForEach 等流式 API 高效处理数据](https://mp.weixin.qq.com/s/7ATm_Zu7ib9MXf8ugy3RcA)\n" \
-                   "3. [优化 Go 二进制文件的大小](https://prog.world/optimizing-the-size-of-the-go-binary)\n" \
-                   "4. [通常是无符号](https://graphitemaster.github.io/aau)\n" \
-                   "5. [将 Go 程序编译为 Nintendo Switch™ 的本机二进制文件](https://ebiten.org/blog/native_compiling_for_nintendo_switch.html)\n" \
-                   "- 编辑: CDS\n" \
-                   "- 订阅新闻: http://tinyletter.com/gocn\n" \
-                   "- 招聘专区: https://gocn.vip/jobs\n" \
-                   "- GoCN 归档: https://gocn.vip/topics/20927"
-    st.markdown(str_markdown)
+    with st.expander("2022-01-25T09:14:39+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Yemeni internet has been restored after a four-day outage")
+            st.write("Yemen’s internet has been restored after a four-day outage. Internet service has been disrupted as a result of strikes on a telecommunications facility in Hudaydah by a Saudi-led coalition. Yemeni capital SANAA According to local residents, Internet service was restored in Yemen early Tuesday after a four-day outage. After Saudi-led coalition airstrikes targeted a telecommunications [...]")
+            st.write("https://infosurhoy.com/news/yemeni-internet-has-been-restored-after-a-four-day-outage/")
+        with col2:
+            st.image("https://infosurhoy.com/wp-content/uploads/newsimages1/thumbs_b_c_bb407951ff2ea006e84dbbbbbaaab1ba.jpg")
+            if st.button('like', key=1):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance ")
+            st.write("Rate")
+
+    with st.expander("2022-01-26T11:03:09+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("What happened with the Apple iCloud outage?")
+            st.write("Panicked users took to Twitter wondering if their accounts had been hacked before the company confirmed it was an outage.The post What happened with the Apple ID outage? appeared first on Silicon Republic.")
+            st.write("https://www.siliconrepublic.com/enterprise/apple-outage-id-icloud-down")
+        with col2:
+            st.image("https://www.siliconrepublic.com/wp-content/uploads/2022/01/icloud-718x523.jpeg")
+            if st.button('like', key=2):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Snowbound Regions of Greece Face Power Outages")
+            st.write("The heavy snowfall in Greece brought about by the storm “Elpis” may cause power outages across regions that have been hit with dense snow. Many regions in Attica, where the city of Athens is located, Evia, and Crete are facing potential power outages due to the snow. Heavy snow causes power outages in Greece According […]Read the full story on GreekReporter.com.")
+            st.write("https://greekreporter.com/2022/01/24/snow-greece-power-outage/")
+        with col2:
+            st.image("https://greekreporter.com/wp-content/uploads/2022/01/athens-snow2-credit-greek-reporter.jpg")
+            if st.button('like', key=3):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Yemeni internet has been restored after a four-day outage")
+            st.write("Yemen’s internet has been restored after a four-day outage. Internet service has been disrupted as a result of strikes on a telecommunications facility in Hudaydah by a Saudi-led coalition. Yemeni capital SANAA According to local residents, Internet service was restored in Yemen early Tuesday after a four-day outage. After Saudi-led coalition airstrikes targeted a telecommunications [...]")
+            st.write("https://infosurhoy.com/news/yemeni-internet-has-been-restored-after-a-four-day-outage/")
+        with col2:
+            st.image("https://infosurhoy.com/wp-content/uploads/newsimages1/thumbs_b_c_bb407951ff2ea006e84dbbbbbaaab1ba.jpg")
+            if st.button('like', key=4):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("What happened with the Apple iCloud outage?")
+            st.write("Panicked users took to Twitter wondering if their accounts had been hacked before the company confirmed it was an outage.The post What happened with the Apple ID outage? appeared first on Silicon Republic.")
+            st.write("https://www.siliconrepublic.com/enterprise/apple-outage-id-icloud-down")
+        with col2:
+            st.image("https://www.siliconrepublic.com/wp-content/uploads/2022/01/icloud-718x523.jpeg")
+            if st.button('like', key=5):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Snowbound Regions of Greece Face Power Outages")
+            st.write("The heavy snowfall in Greece brought about by the storm “Elpis” may cause power outages across regions that have been hit with dense snow. Many regions in Attica, where the city of Athens is located, Evia, and Crete are facing potential power outages due to the snow. Heavy snow causes power outages in Greece According […]Read the full story on GreekReporter.com.")
+            st.write("https://greekreporter.com/2022/01/24/snow-greece-power-outage/")
+        with col2:
+            st.image("https://greekreporter.com/wp-content/uploads/2022/01/athens-snow2-credit-greek-reporter.jpg")
+            if st.button('like', key=6):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Yemeni internet has been restored after a four-day outage")
+            st.write("Yemen’s internet has been restored after a four-day outage. Internet service has been disrupted as a result of strikes on a telecommunications facility in Hudaydah by a Saudi-led coalition. Yemeni capital SANAA According to local residents, Internet service was restored in Yemen early Tuesday after a four-day outage. After Saudi-led coalition airstrikes targeted a telecommunications [...]")
+            st.write("https://infosurhoy.com/news/yemeni-internet-has-been-restored-after-a-four-day-outage/")
+        with col2:
+            st.image("https://infosurhoy.com/wp-content/uploads/newsimages1/thumbs_b_c_bb407951ff2ea006e84dbbbbbaaab1ba.jpg")
+            if st.button('like', key=7):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("What happened with the Apple iCloud outage?")
+            st.write("Panicked users took to Twitter wondering if their accounts had been hacked before the company confirmed it was an outage.The post What happened with the Apple ID outage? appeared first on Silicon Republic.")
+            st.write("https://www.siliconrepublic.com/enterprise/apple-outage-id-icloud-down")
+            st.write("2022-01-26T11:03:09+00:00")
+        with col2:
+            st.image("https://www.siliconrepublic.com/wp-content/uploads/2022/01/icloud-718x523.jpeg")
+            if st.button('like', key=8):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
+
+    with st.expander("2022-01-24T15:54:43+00:00", True):
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader("Snowbound Regions of Greece Face Power Outages")
+            st.write("The heavy snowfall in Greece brought about by the storm “Elpis” may cause power outages across regions that have been hit with dense snow. Many regions in Attica, where the city of Athens is located, Evia, and Crete are facing potential power outages due to the snow. Heavy snow causes power outages in Greece According […]Read the full story on GreekReporter.com.")
+            st.write("https://greekreporter.com/2022/01/24/snow-greece-power-outage/")
+            st.write("2022-01-24T15:54:43+00:00")
+        with col2:
+            st.image("https://greekreporter.com/wp-content/uploads/2022/01/athens-snow2-credit-greek-reporter.jpg")
+            if st.button('like', key=9):
+                st.write('Great!')
+            st.write("BGP Monitor State")
+            st.write("Performance")
+            st.write("Rate")
 elif selected == "User":
     pass
 else:
