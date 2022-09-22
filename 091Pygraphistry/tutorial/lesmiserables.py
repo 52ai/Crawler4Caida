f86ffe61e1e99447c4118ad5a0b82fe778234fce
Wayne YU <ieeflsyu@outlook.com>
2022-09-22 14:39:16 +0800

    开启Graphistry的研究

---
+++ 091Pygraphistry/tutorial/lesmiserables.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+"""
+create on Sep 22, 2022 By Wanye YU
+Email: ieeflsyu@outlook.com
+
+Function:
+以悲惨世界数据集为样本，研究graphistry的可视化
+"""
+import pandas
+links = pandas.read_csv('./lesmiserables.csv')
+print(links)
